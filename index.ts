e3ba7ed0ec93cf63f549f19c77a592a5df7c1c8e
PGamingHD <pontus.2003@hotmail.com>
2026-02-02 18:01:32 +0100

    attach
    attach

--- index.ts
+++ index.ts
@@ -219,10 +219,11 @@ app.post("/player/signup", async (req: Request, res: Response) => {
 
   const token = axiosRes.data.token;
 
-  res.setHeader("Content-Type", "text/html");
-  res.send(
-    `{"status":"success","message":"Account Validated.","token":"${token}","url":"","accountType":"growtopia"}`,
-  );
+  res
+    .send(
+      `{"status":"success","message":"Account Validated.","token":"${token}","url":"","accountType":"growtopia"}`,
+    )
+    .setHeader("Content-Type", "text/html");
 });
 
 app.listen(PORT, () => {
